eb75ebc61d51a9b2f4f991008661908b591a651a
Erin Sullivan <59126916+eesullivan@users.noreply.github.com>
2023-03-22 12:56:39 -0500

    Update ShoppingListPage.java

--- src/main/java/edu/ou/cs/hci/project/panes/ShoppingListPage.java
+++ src/main/java/edu/ou/cs/hci/project/panes/ShoppingListPage.java
@@ -3,7 +3,7 @@ package edu.ou.cs.hci.project.panes;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class ShoppingListPage extends Application {
+public class ShoppingListPage extends AbstractPage {
 
     public static void main(String[] args) {
         launch(args);
