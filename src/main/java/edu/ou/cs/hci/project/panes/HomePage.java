aca0549097b4019a1c16330fc19a84fd09227755
Erin Sullivan <59126916+eesullivan@users.noreply.github.com>
2023-03-22 12:56:10 -0500

    Update HomePage.java

--- src/main/java/edu/ou/cs/hci/project/panes/HomePage.java
+++ src/main/java/edu/ou/cs/hci/project/panes/HomePage.java
@@ -3,7 +3,7 @@ package edu.ou.cs.hci.project.panes;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class HomePage extends Application {
+public class HomePage extends AbstractPage {
 
     public static void main(String[] args) {
         launch(args);
