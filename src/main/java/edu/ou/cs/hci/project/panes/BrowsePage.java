08fcaa8d7a53bdcc3df287c8e5932a2330c724ce
Erin Sullivan <59126916+eesullivan@users.noreply.github.com>
2023-03-22 12:55:55 -0500

    update extend

--- src/main/java/edu/ou/cs/hci/project/panes/BrowsePage.java
+++ src/main/java/edu/ou/cs/hci/project/panes/BrowsePage.java
@@ -3,7 +3,7 @@ package edu.ou.cs.hci.project.panes;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class BrowsePage extends Application {
+public class BrowsePage extends AbstractPage {
 
     public static void main(String[] args) {
         launch(args);
