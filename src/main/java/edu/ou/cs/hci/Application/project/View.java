4cf11695f9e635a5d51262bbc49bf1e13e412562
Erin Sullivan <59126916+eesullivan@users.noreply.github.com>
2023-04-28 13:32:27 -0500

    clean view

--- src/main/java/edu/ou/cs/hci/Application/project/View.java
+++ src/main/java/edu/ou/cs/hci/Application/project/View.java
@@ -17,18 +17,15 @@ import edu.ou.cs.hci.Application.project.panes.*;
 
 public class View
 {
-    //**********************************************************************
+  
     // Private Class Members (Layout)
-    //**********************************************************************
-
+ 
     private static final double	SCENE_W = 960;		// Scene width
     private static final double	SCENE_H = 540;		// Scene height
 
-    //**********************************************************************
+   
     // Private Members
-    //**********************************************************************
-
-    // Master of the program, manager of the data, mediator of all updates
+    
     private final Controller				controller;
     private static TabPane                     tabPane;
 
@@ -42,35 +39,32 @@ public class View
     private final Stage					stage;
     private final ArrayList<AbstractPane>	panes;
 
-    //**********************************************************************
     // Constructors and Finalizer
-    //**********************************************************************
 
-    // Construct a scene and display it in a window (stage).
+    // Construct a scene and display
     public View(Controller controller, String name, double x, double y)
     {
         this.controller = controller;
 
-        // Create a listener for various widgets that emit ActionEvents
+        // Create a listener 
         actionHandler = new ActionHandler();
 
-        // Create a listener to handle WINDOW_CLOSE_REQUESTs
         windowHandler = new WindowHandler();
 
-        // Create a set of panes to include
+        // Create a set of panes
         panes = new ArrayList<AbstractPane>();
 
-        // Construct the pane for the scene.
+        // Construct the pane 
         Pane	view = buildView();
 
-        // Create a scene with an initial size, and attach a style sheet to it
+        // Create a scene
         Scene		scene = new Scene(view, SCENE_W, SCENE_H + 32);
         URL		url = View.class.getResource("View.css");
         String		surl = url.toExternalForm();
 
         scene.getStylesheets().add(surl);
 
-        // Create a window/stage with a name and an initial position on screen
+        // Create a window/stage
         stage = new Stage();
 
         stage.setOnHiding(windowHandler);
@@ -81,20 +75,16 @@ public class View
         stage.show();
     }
 
-    //**********************************************************************
-    // Public Methods (Controller)
-    //**********************************************************************
 
-    // The controller calls this method when it adds a view.
-    // Set up the nodes in the view with data accessed through the controller.
+    // Public Methods (Controller)
+
     public void	initialize()
     {
         for (AbstractPane pane : panes)
             pane.initialize();
     }
 
-    // The controller calls this method when it removes a view.
-    // Unregister event and property listeners for the nodes in the view.
+    
     public void	terminate()
     {
         for (AbstractPane pane : panes)
@@ -102,32 +92,29 @@ public class View
 
     }
 
-    // The controller calls this method whenever something changes in the model.
-    // Update the nodes in the view to reflect the change.
     public void	update(String key, Object value)
     {
         for (AbstractPane pane : panes)
             pane.update(key, value);
 
-        // Update your menus as needed when any old model properties change...
+    
     }
 
-    // The controller calls this method whenever something changes in the model.
-    // Update the nodes in the view to reflect the change.
+
     public void	updateProperty(String key, Object newValue, Object oldValue)
     {
         for (AbstractPane pane : panes)
             pane.updateProperty(key, newValue, oldValue);
 
-        // Update your menus as needed when any new model observables change...
+        
     }
 
     public static TabPane getTabPane() {
         return tabPane;
     }
-    //**********************************************************************
+
     // Private Methods (Layout)
-    //**********************************************************************
+  
 
     private Pane	buildView()
     {
