5668cb70a19c4c6a0e6d126c893eeac871c49089
Erin Sullivan <59126916+eesullivan@users.noreply.github.com>
2023-04-28 13:34:29 -0500

    clean abstract

--- src/main/java/edu/ou/cs/hci/Application/project/panes/AbstractPane.java
+++ src/main/java/edu/ou/cs/hci/Application/project/panes/AbstractPane.java
@@ -16,14 +16,14 @@ import javafx.scene.layout.*;
 import edu.ou.cs.hci.Application.project.Controller;
 import edu.ou.cs.hci.Application.project.resources.*;
 import edu.ou.cs.hci.Application.project.View;
-//******************************************************************************
+
 
 @SuppressWarnings("unchecked")
 public abstract class AbstractPane
 {
-    //**********************************************************************
+ 
     // Public Class Members
-    //**********************************************************************
+   
     public static final String[] CATEGORIES = {
             "Fruits", "Vegetables", "Protein/Alternatives",
             "Dairy/Alternatives", "Grains", "Fats/Oils/Spices"
@@ -34,11 +34,9 @@ public abstract class AbstractPane
     public static final String	FX_ICON	= RSRC + "example/fx/icon/";
     public static final String	FX_TEXT	= "example/fx/text/";
 
-    //**********************************************************************
+  
     // Private Members
-    //**********************************************************************
 
-    // Master of the program, manager of the data, mediator of all updates
     protected final Controller	controller;
     protected final String		name;
     protected final String		hint;
@@ -49,9 +47,9 @@ public abstract class AbstractPane
     // Provided when the subclass constructor calls setBase()
     protected Node				base;
 
-    //**********************************************************************
+
     // Constructors and Finalizer
-    //**********************************************************************
+   
 
     protected AbstractPane(Controller controller, String name, String hint)
     {
@@ -65,9 +63,8 @@ public abstract class AbstractPane
         selectedIngredients = new ArrayList<>();
     }
 
-    //**********************************************************************
     // Getters and Setters
-    //**********************************************************************
+   
 
     public String	getName()
     {
@@ -90,9 +87,9 @@ public abstract class AbstractPane
         this.base = base;
     }
 
-    //**********************************************************************
+
     // Public Methods
-    //**********************************************************************
+  
 
     // Convenience method to put the base node into a tab. Warning: Don't
     // attempt to layout both the base node and tabs containing it! (Each
@@ -102,36 +99,32 @@ public abstract class AbstractPane
         return createFixedTab(base, name, hint);
     }
 
-    //**********************************************************************
+ 
     // Public Methods (Controller)
-    //**********************************************************************
+    
 
-    // The controller calls this method when it adds a view.
-    // Set up the nodes in the view with data accessed through the controller.
+   
     public void	initialize()
     {
     }
 
-    // The controller calls this method when it removes a view.
-    // Unregister event and property listeners for the nodes in the view.
+    
     public void	terminate()
     {
     }
 
-    // The controller calls this method whenever something changes in the model.
-    // Update the nodes in the view to reflect the change.
+    
     public void	update(String key, Object value)
     {
     }
-    // The controller calls this method whenever something changes in the model.
-    // Update the nodes in the view to reflect the change.
+   
     public void	updateProperty(String key, Object newValue, Object oldValue)
     {
     }
 
-    //**********************************************************************
+
     // Public Class Methods (Resources)
-    //**********************************************************************
+   
 
     // WARNING: May not be a long-term solution
     // Works as intended now, but will need to monitor once we start adding listeners
@@ -160,7 +153,6 @@ public abstract class AbstractPane
     }
 
     // Convenience method to create a node for an image located in resources
-    // relative to the SWING_ICON package. See static member definitions above.
     public static ImageView	createSwingIcon(String url, int size)
     {
         Image	image = new Image(SWING_ICON + url, size, size, false, false);
@@ -169,7 +161,6 @@ public abstract class AbstractPane
     }
 
     // Convenience method to create a node for an image located in resources
-    // relative to the FX_ICON package. See static member definitions above.
     public static ImageView	createFXIcon(String url, double w, double h)
     {
         Image	image = new Image(FX_ICON + url, w, h, false, true);
@@ -178,15 +169,13 @@ public abstract class AbstractPane
     }
 
     // Convenience method to slurp a CSV formatted file located in resources
-    // relative to the FX_TEXT package. See static member definitions above.
     public static List<List<String>>	loadFXData(String url)
     {
         return Resources.getCSVData(FX_TEXT + url);
     }
 
-    //**********************************************************************
+
     // Public Class Methods (Layout)
-    //**********************************************************************
 
     // Convenience method to create an unclosable tab with a tooltip.
     public static Tab	createFixedTab(Node node, String title, String text)
@@ -213,8 +202,7 @@ public abstract class AbstractPane
         return node;
     }
 
-    // JavaFX doesn't have a Swing-style TitledBorder, so use a TitledPane.
-    // TitledPanes usually go in Accordians, but can be used independently.
+
     public static Pane	createTitledPane(Region region, String title)
     {
         StackPane	stackPane = new StackPane(region);
@@ -228,8 +216,6 @@ public abstract class AbstractPane
         return new StackPane(titledPane);
     }
 
-    // This is the procedural version. The declarative CSS version would look
-    // like, e.g. region.setStyle("-fs-background-color: white;")
     public static void	setBackgroundColor(Region region, Color color)
     {
         BackgroundFill	bf = new BackgroundFill(color, null, null);
